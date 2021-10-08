c84b48f54c82a4625233c160a3f76ba4a5d6b809
Ricardo Farias <ricardo14231@hotmail.com>
2021-10-08 11:15:44 -0300

    Adiciona todos os atibutos na classe LerArquivo

--- src/main/java/com/ricardofarias/ctc/main/FrasesMaisFrequentesMain.java
+++ src/main/java/com/ricardofarias/ctc/main/FrasesMaisFrequentesMain.java
@@ -1,92 +1,13 @@
 package com.ricardofarias.ctc.main;
 
 import com.ricardofarias.ctc.utils.LerArquivo;
-import com.ricardofarias.ctc.utils.OrdenaFrase;
-import com.ricardofarias.ctc.utils.SalvaArquivo;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class FrasesMaisFrequentesMain {
 
-    private static Map<String, Integer> mapAux = new HashMap<>();
-
-    private static Map<String, Integer> mapFraseAtual = new HashMap<>();
-    private static Map<String, Integer> mapFraseFinal = new HashMap<>();
-
     public static void main(String[] args) {
-            long index = 0;
 
-        long valorAnt = 0;
-        long valorAtual = -1;
-int j = 0;
-        while (j != 7){
-            j++;
-            index = LerArquivo.lerArquivoBaseDeDados(mapAux, index);
-            mapAux.forEach((key, value) -> mapFraseAtual.merge(key, value, (v1, v2) -> v1+v2));
-            mapAux.clear();
+        LerArquivo.lerArquivoBaseDeDados();
 
-            mapFraseFinal = OrdenaFrase.ordenaPorQuantidadeFrase(mapFraseAtual);
-            //System.out.println(valorAnt+ "   " + valorAtual + "   " + index);
-
-            //valorAtual = index;
-            if(valorAnt == valorAtual) {
-                //System.out.println("Foi" + " index: " + index);
-             //   break;
-            }
-            valorAnt = valorAtual;
-        }
-        //SalvaArquivo.salvaArquivoFinal(mapFraseFinal);
-        imprime(mapFraseFinal);
-
-
-
-        /*
-        //IMPRIME CADA FRASE
-        imprime(mapFraseAtual);
-        System.out.println("___________________________________");
-*/
-        //mapParte2ArquivoSegemento.values().stream().forEach(System.out::println);
-
-        //frasesMaisFrequentes = OrdenaFrase.ordenaPorQuantidadeFrase(frasesMaisFrequentes);
-
-
-        //SalvaArquivo.salvaArquivoFrases(frasesMaisFrequentes, "fraseResultado.txt");
-        //IMPRIME A QUANTIDADE DE CADA FRASE
-
-
-
-        //TODO chamar só uma vez. Gera o arquivo com as frases.
-        //GeraArquivoTeste.geraArquivo();
-    }
-/*
-    //TODO finalizar
-    private static boolean verificaIndexArray1ComArray2(Map<String, Integer> map1, Map<String, Integer> map2) {
-
-        int ultimoIndex = map2.size() - 1;
-        //System.out.println(ultimoIndex);
-        //TODO acessar o ultimo elemento para não precisar passar pela ordenação.
-        if(map1.values().stream().min((o1, o2) -> o2).get() > map2.values().stream().max((o1, o2) -> o1).get()){
-            //return true;
-
-        }
-        System.out.println(map1.values().stream().min((o1, o2) -> o2).get());
-        System.out.println(map2.values().stream().max((o1, o2) -> o1).get());
-        System.out.println(map2.get(0));
-        return false;
     }
 
-    private static void substituiMaiorValorArrayComArray2(Map<String, Integer> map1, Map<String, Integer> map2) {
-        for (Map.Entry<String,Integer> frase : map1.entrySet()) {
-            //frase.getValue() < map2.
-        }
-    }
-*/
-
-    //TODO temporário
-    private static void imprime(Map<String, Integer> mapFrase) {
-        for (Map.Entry<String,Integer> frase : mapFrase.entrySet()) {
-            System.out.println(frase.getKey() +" " + frase.getValue());
-        }
-    }
 }
--- src/main/java/com/ricardofarias/ctc/utils/LerArquivo.java
+++ src/main/java/com/ricardofarias/ctc/utils/LerArquivo.java
@@ -4,21 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LerArquivo {
-    static final String PATHBASEDADOS = "tt.txt";
+    private static Map<String, Integer> mapAux = new HashMap<>();
+    private static Map<String, Integer> mapFraseAtual = new HashMap<>();
+    private static Map<String, Integer> mapFraseFinal = new HashMap<>();
 
-    public static long lerArquivoBaseDeDados(Map<String, Integer> mapFrases, long indexLinhaNoArquivo) {
+    private static final String PATHBASEDADOS = "basedados-frases.txt";
+
+    public static void lerArquivoBaseDeDados() {
 
         long indexLinhaAtual = 0;
         long linhasLidas = 0;
         List<String> frasesPorLinha = null;
         String linha;
+        long indexLinhaNoArquivo = 0;
+        boolean linhasLidasForMaior1000 = false;
 
         File file = new File(PATHBASEDADOS);
 
@@ -26,8 +29,7 @@ public class LerArquivo {
              BufferedReader br = new BufferedReader (fr, 1024)) {
 
             br.skip(indexLinhaNoArquivo);
-            //System.out.println(br.readLine());
-           // System.out.println(br.read());
+
             while ((linha = br.readLine()) != null) {
                 //TODO soma 2 pois é os caractere /n de fim de linha.
                 indexLinhaAtual += linha.length() + 2;
@@ -37,33 +39,40 @@ public class LerArquivo {
                     frasesPorLinha = retornaArrayFrase(linha);
 
                     for (int i=0; i < frasesPorLinha.size(); i++){
-                        mapFrases.put(frasesPorLinha.get(i), mapFrases.getOrDefault(frasesPorLinha.get(i), 0) + 1);
+                        mapAux.put(frasesPorLinha.get(i), mapAux.getOrDefault(frasesPorLinha.get(i), 0) + 1);
                     }
-                    //frasesPorLinha.clear();
                 }
-                System.out.println("main");
 
-                if(linhasLidas >= 1) {
-                    //linhasLidas = 0;
-                    //break;
-                    test();
+                if(linhasLidas >= 10) {
+                    linhasLidasForMaior1000 = true;
+                    SalvaAsPrimeiras80kFrases();
                 }
-                //System.out.println(linha);
             }
-           // System.out.println(frasesPorLinha.size());
-           // if(frasesPorLinha.size() == 0) return -1;
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //TODO se não entra no if de linhas
+        if(!linhasLidasForMaior1000)
+        SalvaAsPrimeiras80kFrases();
+        imprime(mapFraseFinal);
+        SalvaArquivo.salvaArquivoFinal(mapFraseFinal);
 
-        return indexLinhaAtual;
     }
-    private static void test(){
-        int i = 0;
-        while (i < 1000000) i++;
-        System.out.println("TESTE");
+
+    private static void SalvaAsPrimeiras80kFrases(){
+        mapAux.forEach((key, value) -> mapFraseAtual.merge(key, value, (v1, v2) -> v1+v2));
+        mapAux.clear();
+
+        mapFraseFinal = OrdenaFrase.ordenaPorQuantidadeFrase(mapFraseAtual);
     }
+
+    //TODO temporário
+    private static void imprime(Map<String, Integer> mapFrase) {
+        for (Map.Entry<String,Integer> frase : mapFrase.entrySet()) {
+            System.out.println(frase.getKey() +" " + frase.getValue());
+        }
+    }
+
     private static List<String> retornaArrayFrase(String linha) {
         return Arrays.stream(linha.split("\\|")).collect(Collectors.toList());
     }
